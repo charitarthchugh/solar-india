57cce9bef73c4d0cfb28bffa6ff49b75d11e530a
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-11-22 07:45:17 -0500

    Update git_push.py

--- git_push.py
+++ git_push.py
@@ -1,7 +1,13 @@
 import os
 from git import Repo
+import jovian
 git_dir = os.getcwd()+ "/.git"
 if(os.path.exists(git_dir)):
+    jovian.commit(
+        environment="conda",
+        project="solar-india",
+        filename="solar-india.ipynb",
+        git_commit=True,)
     try:
             repo = Repo(git_dir)
             origin =repo.remote(name="origin")
